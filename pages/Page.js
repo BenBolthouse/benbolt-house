bda85a1bcb20da635abe014cc28b7cd65495c20d
Ben Bolthouse <benbolthouse@gmail.com>
2021-04-08 12:32:40 -0700

    Add page class and integrate into app and template

---
+++ pages/Page.js
@@ -0,0 +1,26 @@
+class Page {
+  constructor(view, options) {
+    this.view = view;
+    this.config = {
+      head: {
+        title: options.title || 'My Website',
+        description: options.description || 'Page Description',
+        noindex: options.noindex !== undefined ? options.noindex : false,
+        keywords: options.keywords !== undefined
+          ? this.formatKeywordsFromArray(options.keywords)
+          : [],
+      },
+    };
+  }
+  formatKeywordsFromArray(array) {
+    let out = '';
+    if (array.length) {
+      array.forEach((k) => {
+        out += `${k.toLowerCase()},`;
+      });
+    }
+    return out.slice(0, out.length - 1);
+  }
+}
+
+module.exports = Page;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
+const Page = require('../pages/Page');
+
 const router = require('express').Router();
 
+/**
+ * GET: "/" --> Return home page of the website
+ */
 router.get('/', (_req, res, _next) => {
-  res.render('home');
+  const page = new Page('home', {
+    title: 'Ben Bolthouse',
+    description: 'Welcome to my website where I share my projects and ideas. Grab some coffee and come hang out!',
+    keywords: ['Software Engineer', 'Node', 'Express'],
+  });
+  res.render(page.view, { ...page.config });
 });
 
 module.exports = router;
