ac21f3645b737bb3422efdf041eb98410be29700
Ben Bolthouse <benbolthouse@gmail.com>
2021-04-08 19:43:10 -0700

    Move page data to json data directory

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,13 @@
-const Page = require('../models/Page');
-
 const router = require('express').Router();
+
+const data = require('../data');
+const Page = require('../models/Page');
 
 /**
  * GET: "/" --> Return home page of the website
  */
 router.get('/', (_req, res, _next) => {
-  const page = new Page('home', {
-    title: 'Ben Bolthouse',
-    description: 'Welcome to my website where I share my projects and ideas. Grab some coffee and come hang out!',
-    keywords: ['Software Engineer', 'Node', 'Express'],
-  });
+  const page = new Page('home', data.home);
   res.render(page.view, { ...page.config });
 });
 
