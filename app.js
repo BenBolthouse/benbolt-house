eac133e7b0e5fb7ae8cc9d4b486f4a994b6f5da3
Ben Bolthouse <benbolthouse@gmail.com>
2021-04-12 12:47:22 -0700

    Add dev dependencies for browser reloads

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const { httpLogger } = require('./log');
-const config = require('./config');
 const express = require('express');
 const expressHbs = require('express-handlebars');
 const favicon = require('serve-favicon');
+const livereload = require('livereload').createServer();
+const connectLivereload = require('connect-livereload');
 const path = require('path');
 const Prismic = require('prismic-javascript');
 const prismicConfig = require('./config/prismic');
@@ -11,6 +12,8 @@ const prismicConfig = require('./config/prismic');
 const routes = require('./routes');
 
 const app = express();
+
+livereload.watch(path.join(__dirname, 'views'));
 
 // middleware configures express to use prismic cms repo
 app.use((req, res, next) => {
@@ -30,11 +33,19 @@ app.use((req, res, next) => {
     .catch(error => next(error.message));
 });
 
+// for development: live reload on changes to views directory
+livereload.server.once('connection', () => {
+  setTimeout(() => {
+    livereload.refresh('/');
+  }, 100);
+});
+
 app.engine('handlebars', expressHbs());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(favicon(path.join(__dirname, 'static', 'favicon.ico')));
+app.use(connectLivereload());
 app.use(httpLogger);
 
 // application routes
