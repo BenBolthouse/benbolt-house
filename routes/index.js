9db1c49a840dd060f23b13c2b552b2d4c82b01d3
Ben Bolthouse <benbolthouse@gmail.com>
2021-05-03 11:58:58 -0700

    Add api route to collect form data

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,50 @@
 const { google } = require('googleapis');
 const { googleSheetId } = require('../config');
 const { jwtClient } = require('../config/google');
-const csurf = require('csurf')({ cookie: true });
+const asyncHandler = require('express-async-handler');
 const home = require('../data/home.json');
 const Page = require('../lib/Page');
 const router = require('express').Router();
 
-// configure for google sheets
-const sheets = google.sheets({
-  version: 'v4',
-});
+// Following line is required by googleapis to initialize access to
+// spreadsheets.
+const sheets = google.sheets({ version: 'v4' });
 
-router.get('/', csurf, (req, res, _next) => {
+// GET https://benbolt.house/
+// Renders and sends the home view for the website.
+router.get('/', (req, res, _next) => {
   const csrf = req.csrfToken();
   const page = new Page('home', home);
   res.render(page.view, { ...page.config, csrf });
 });
 
-router.post('/', csurf, async (req, res, next) => {
-  // prettier-ignore
-  const {
-    email,
-    phone,
-    name,
-    body,
-  } = req.body;
+// POST https://benbolt.house/api/v1/contact
+// Accepts AJAX post request with contact form data and persists to an
+// external store.
+router.post('/api/v1/contact', asyncHandler(async (req, res, next) => {
   const params = {
     spreadsheetId: googleSheetId,
     auth: jwtClient,
     range: 'contacts',
   };
-  const get = cb =>
-    sheets.spreadsheets.values.get(params).then(
-      (result) => {
-        cb(result.data.values);
-      },
-      err => next(err),
-    );
-  const update = values =>
-    sheets.spreadsheets.values
-      .update({
-        ...params,
-        valueInputOption: 'USER_ENTERED',
-        resource: {
-          values: [...values, ['XXX', email, phone, name, body]],
-        },
-      })
-      .then(
-        result => true,
-        err => next(err),
-      );
-  get(update);
-});
+  const cells = [
+    req.body.email,
+    req.body.phone,
+    req.body.name,
+    req.body.body,
+  ];
+  const spreadsheet = await sheets.spreadsheets.values.get(params);
+  const response = await sheets.spreadsheets.values.update({
+    ...params,
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      values: [...spreadsheet.data.values, [new Date().toISOString(), ...cells]],
+    },
+  });
+  if (response.status === 200) {
+    res.status(200).json({ message: 'Success' });
+  }
+  res.status(400).json({ message: 'Error' });
+}));
 
 module.exports = router;
